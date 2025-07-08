b8a6389de048310ac78cc17461bcd0e2535f32fc
harunar21 <harun@rekosistem.com>
2025-07-08 23:23:53 +0700

    Changes: - Feat: Add unit test

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,8 @@
 import type { IRepo, IUser } from "@/types";
 
-const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-
-const headers: HeadersInit = {
-  Authorization: `Bearer ${GITHUB_TOKEN}`,
-};
 export const searchUsers = async (query: string): Promise<IUser[]> => {
   const res = await fetch(
     `https://api.github.com/search/users?q=${query}&per_page=5`,
-    {
-      headers,
-    },
   );
   if (!res.ok) throw new Error(`GitHub API error: ${res.status}`);
   const data = await res.json();
@@ -18,9 +10,7 @@ export const searchUsers = async (query: string): Promise<IUser[]> => {
 };
 
 export const fetchRepo = async (username: string): Promise<IRepo[]> => {
-  const res = await fetch(`https://api.github.com/users/${username}/repos`, {
-    headers,
-  });
+  const res = await fetch(`https://api.github.com/users/${username}/repos`);
   if (!res.ok) throw new Error(`Failed to fetch repositories for ${username}`);
   const data = await res.json();
   return data || [];
